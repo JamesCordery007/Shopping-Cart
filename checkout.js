c75cefb4e22b16b29a8390c3c366d50eed1f3db1
JohnB3787 <140168540+JohnB3787@users.noreply.github.com>
2023-09-08 16:42:35 +0100

    A full working template for development and testing,inportant to look at the amount being sent to the payment request it is 2 decimal points under the main price

---
+++ checkout.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Retrieve cart data from localStorage
+    const savedCart = JSON.parse(localStorage.getItem('cart'));
+    const savedTotalAmount = localStorage.getItem('totalAmount');
+
+    // Update checkout page with cart data
+    if (savedCart) {
+        savedCart.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `${item.name} - £${item.price}`;
+            document.getElementById('checkoutCart').appendChild(li);
+        });
+    }
+
+    // Update total amount
+    if (savedTotalAmount) {
+        document.getElementById('checkoutTotalAmount').textContent = savedTotalAmount;
+    }
+});
+
+function addToOrder() {
+    // Gather shipping information
+    const name = document.getElementById('name').value;
+    const address1 = document.getElementById('address1').value;
+    const address2 = document.getElementById('address2').value;
+    const town = document.getElementById('town').value;
+    const county = document.getElementById('county').value;
+    const postcode = document.getElementById('postcode').value;
+
+    // Calculate the total amount from the cart
+    const savedTotalAmount = localStorage.getItem('totalAmount');
+    if (savedTotalAmount) {
+        document.getElementById('checkoutTotalAmount').textContent = savedTotalAmount;
+    }
+
+    // Enable the "Buy Now" button
+    document.getElementById('buyNowButton').removeAttribute('disabled');
+}
+
+function buyNow() {
+    // Gather shipping information
+    const name = document.getElementById('name').value;
+    const address1 = document.getElementById('address1').value;
+    const address2 = document.getElementById('address2').value;
+    const town = document.getElementById('town').value;
+    const county = document.getElementById('county').value;
+    const postcode = document.getElementById('postcode').value;
+
+    // Calculate the total amount from the cart
+    const savedTotalAmount = localStorage.getItem('totalAmount');
+
+    // Prepare data to send to the PHP script
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('address1', address1);
+    formData.append('address2', address2);
+    formData.append('town', town);
+    formData.append('county', county);
+    formData.append('postcode', postcode);
+    formData.append('totalAmount', savedTotalAmount);
+
+    // Send data to the PHP script using fetch
+    fetch('payment.php', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.text())
+    .then(data => {
+        // Handle the response from the PHP script
+        console.log(data);
+        // You can redirect to a thank you page or perform further actions here
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
---
+++ payment.php
@@ -0,0 +1,75 @@
+<?php
+
+$username = 'iuospcpc0onok906'; // Basic Auth username
+$password = 'tf5zohhzsl7erll1prui7pz12d2tzq6q09g2bfqwajah1qicc3fmnnhqfq72m0bi'; // Basic Auth password
+$url = 'https://try.access.worldpay.com/payment_pages';
+
+// Retrieve address details from the checkout page
+$name = $_POST['name'];
+$address1 = $_POST['address1'];
+$address2 = $_POST['address2'];
+$town = $_POST['town'];
+$county = $_POST['county'];
+$postcode = $_POST['postcode'];
+
+// Calculate the total amount from the checkout page
+$totalAmount = $_POST['totalAmount'];
+
+// JSON data
+$jsonData = array(
+    "transactionReference" => "Memory265-13/08/1876",
+    "merchant" => array(
+        "entity" => "MindPalaceLtd"
+    ),
+    "narrative" => array(
+        "line1" => "Mind Palace Ltd"
+    ),
+    "value" => array(
+        "currency" => "GBP",
+        "amount" => $totalAmount
+    ),
+    // Add address details
+    "address" => array(
+        "name" => $name,
+        "address1" => $address1,
+        "address2" => $address2,
+        "town" => $town,
+        "county" => $county,
+        "postcode" => $postcode
+    )
+);
+
+$jsonPayload = json_encode($jsonData);
+
+// Set headers
+$headers = array(
+    'Content-Type: application/vnd.worldpay.payment_pages-v1.hal+json',
+    'Accept: application/vnd.worldpay.payment_pages-v1.hal+json'
+);
+
+// Initialize cURL session
+$ch = curl_init();
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonPayload);
+
+// Execute cURL session and capture the response
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if (curl_errno($ch)) {
+    echo 'Curl error: ' . curl_error($ch);
+} else {
+    echo 'Response: ' . $response;
+}
+
+// Close cURL session
+curl_close($ch);
+
+?>
+
